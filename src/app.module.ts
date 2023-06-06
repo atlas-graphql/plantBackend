3f488361a80a85ff8c4d9cdced6168c84906bc64
Eigilak <hello@eigilak.dk>
2023-06-07 00:26:22 +0200

    connect with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
 import { PlantsModule } from './plants/plants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log({ configService: configService.get('POSTGRES_HOST') });
+        return {
+          type: 'postgres',
+          host: configService.get('POSTGRES_HOST'),
+          port: configService.get('POSTGRES_PORT'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
+          entities: [__dirname + '/../**/* entity. (js,ts)'],
+          synchronize: true,
+        };
+      },
+    }),
     PlantsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
