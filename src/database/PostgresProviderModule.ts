71f8b5cb016a44f6fd1ccc0ea10cd05da0b76c6c
Eigilak <hello@eigilak.dk>
2023-07-19 17:06:10 +0200

    fix

--- src/database/PostgresProviderModule.ts
+++ src/database/PostgresProviderModule.ts
@@ -8,13 +8,13 @@ import { Plant } from '../plants/entities/plant.entity';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: () => ({
         type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'postgres',
         entities: [Plant],
         autoLoadEntities: true,
         synchronize: true,
